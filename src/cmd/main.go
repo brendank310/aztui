2b78944595420bf42f3571ce990ad04fa74216a6
adrianabedon <adrianabedon@microsoft.com>
2024-09-16 16:17:04 -0700

    create simple logger

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/brendank310/aztui/pkg/azcli"
 	"github.com/brendank310/aztui/pkg/config"
 	"github.com/brendank310/aztui/pkg/layout"
+	"github.com/brendank310/aztui/pkg/logger"
 	"github.com/brendank310/aztui/pkg/resourceviews"
 
 	_ "github.com/rivo/tview"
@@ -21,6 +22,10 @@ type AzTuiState struct {
 
 func NewAzTuiState() *AzTuiState {
 	// Base initialization
+	err := logger.InitLogger()
+	if err != nil {
+		panic(err)
+	}
 	a := AzTuiState{
 		AppLayout: layout.NewAppLayout(),
 	}
@@ -42,11 +47,10 @@ func NewAzTuiState() *AzTuiState {
 	return &a
 }
 
-
 func main() {
 	a := NewAzTuiState()
 
 	if err := a.AppLayout.App.SetRoot(a.AppLayout.Grid, true).Run(); err != nil {
-			panic(err)
+		panic(err)
 	}
 }
